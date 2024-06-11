0a7967636b4ed922d29e199c3aeeca4fbf5fff5b
AzizbekEngineer <azizbekengineer@gmail.com>
2024-06-11 16:36:01 +0500

    add

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,17 +1,22 @@
-import React from 'react'
-import "./Navbar.css"
-import { NavLink } from 'react-router-dom'
+import React from "react";
+import useStore from "../../store/store";
+
+import "./Navbar.css";
+import { NavLink } from "react-router-dom";
 
 function Navbar() {
+  let user = useStore((state) => state.user);
 
   return (
-    <div className='navbar'>
-        <h2>Redux Toolkit</h2>
-        <NavLink to={"/"}>Home</NavLink>
-        <NavLink to={"/create-user"}>Create user</NavLink>
-        <NavLink to={"/all-users"}>All users <sup>0</sup></NavLink>
+    <div className="navbar">
+      <h2>Redux Toolkit</h2>
+      <NavLink to={"/"}>Home</NavLink>
+      <NavLink to={"/create-user"}>Create user</NavLink>
+      <NavLink to={"/all-users"}>
+        All users <sup>{user.length}</sup>
+      </NavLink>
     </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
